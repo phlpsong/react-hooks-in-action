8e7e791ad2474d8082dd2b6c67a3d97d529b59e7
phlpsong <philsongfei@gmail.com>
2023-02-15 17:49:01 +0800

    09-03-useBookings

--- src/components/bookings/BookingsGrid.tsx
+++ src/components/bookings/BookingsGrid.tsx
@@ -2,39 +2,18 @@ import {useEffect, useMemo, useState, Fragment} from 'react';
 import {getGrid, transformBookings} from './grid-builder';
 import {getBookings} from '../../utils/api';
 import Spinner from '../ui/Spinner';
+import { useBookings, useGrid } from './bookingsHook';
 
 export default function BookingsGrid (
   {week, bookable, booking, setBooking}
 ) {
-  const [bookings, setBookings] = useState(null);
-  const [error, setError] = useState(false);
+  const {bookings, status, error} = useBookings(bookable?.id, week.start, week.end);
 
-  const {grid, sessions, dates} = useMemo(
-
-    () => bookable ? getGrid(bookable, week.start) : {},
-
-    [bookable, week.start]
-  );
+  const {grid, sessions, dates} = useGrid(bookable, week.start);
 
   useEffect(() => {
-    if (bookable) {
-      let doUpdate = true;
-
-      setBookings(null);
-      setError(false);
-      setBooking(null);
-
-      getBookings(bookable.id, week.start, week.end)
-        .then(resp => {
-          if (doUpdate) {
-            setBookings(transformBookings(resp));
-          }
-        })
-        .catch(setError);
-
-      return () => doUpdate = false;
-    }
-  }, [week, bookable, setBooking]);
+    setBooking(null);
+  }, [bookable, week.start, setBooking]);
 
   function cell (session, date) {
     const cellData = bookings?.[session]?.[date]
@@ -46,8 +25,8 @@ export default function BookingsGrid (
     return (
       <td
         key={date}
-        className={isSelected ? 'selected' : null}
-        onClick={bookings ? () => setBooking(cellData) : null}
+        className={isSelected ? 'selected' : undefined}
+        onClick={status === 'success' ? () => setBooking(cellData) : undefined}
       >
         {cellData.title}
       </td>
@@ -66,7 +45,7 @@ export default function BookingsGrid (
         </p>
       )}
       <table
-        className={bookings ? 'bookingsGrid active' : 'bookingsGrid'}
+        className={status === 'success'  ? 'bookingsGrid active' : 'bookingsGrid'}
       >
         <thead>
           <tr>

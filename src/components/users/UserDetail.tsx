50a9a43e4676a0d5d16dd00f986eaa3078e28591
phlpsong <philsongfei@gmail.com>
2023-02-21 17:00:01 +0800

    12-02-prefetch-query

--- src/components/users/UserDetail.tsx
+++ src/components/users/UserDetail.tsx
@@ -1,18 +1,29 @@
-export default function UserDetail ({ user }) {
+import { useQuery } from 'react-query';
+import getData from '../../utils/api';
+import Avatar from './Avatar';
+
+export default function UserDetail ({ userID }) {
+  const {data: user} = useQuery(
+    ['user', userID],
+    () => getData(`http://localhost:3001/users/${userID}`),
+    {suspense: true}
+  );
   return (
-    <div>
-      {user && (
-        <div className="bookable-details">
-          <div className="item">
-            <div className="item-header">
-              <h2>
-                {user.title}
-              </h2>
-            </div>
-            <p>{user.notes}</p>
-          </div>
-        </div>
-      )}
+    <div className="item user">
+      <div className="item-header">
+        <h2>{user.name}</h2>
+      </div>
+
+      <Avatar
+        src={`http://localhost:3001/img/${user.img}`}
+        fallbackSrc="http://localhost:3001/img/avatar.gif"
+        alt={user.name}
+      />
+
+      <div className="user-details">
+        <h3>{user.title}</h3>
+        <p>{user.notes}</p>
+      </div>
     </div>
   );
 }
--- src/components/users/UsersPage.tsx
+++ src/components/users/UsersPage.tsx
@@ -1,24 +1,44 @@
-import { Fragment, useState } from 'react';
+import { Fragment, Suspense, useState } from 'react';
+import { useQueryClient } from 'react-query';
+import getData from '../../utils/api';
+import PageSpinner from '../ui/PageSpinner';
 import UserDetail from './UserDetail';
 import { useUser } from './UserProvider';
 import UsersList from './UsersList';
 
 export default function UsersPage () {
-  const [user, setUser] = useState(null);
-  // const {user: loggedInUser} = useContext(UserContext);
-
   const [loggedInUser] = useUser();
+
+  const [selectedUser, setSelectedUser] = useState(null);
+  const queryClient = useQueryClient();
 
   // if no user has been selected in the users list,
   // select the logged in user
-  const currentUser = user || loggedInUser;
+  const user = selectedUser || loggedInUser;
 
-  return (
+  function switchUser(nextUser) {
+    setSelectedUser(nextUser);
+    queryClient.prefetchQuery(
+      ['user', nextUser.id],
+      () => getData(`http://localhost:3001/users/${nextUser.id}`)
+    );
+
+    queryClient.prefetchQuery(`http://localhost:3001/img/${nextUser.img}`, () => new Promise((resolve) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.src = `http://localhost:3001/img/${nextUser.img}`;
+    }
+    ));
+  }
+
+  return user ? (
     <main className="users-page">
       <Fragment>
-        <UsersList user={currentUser} setUser={setUser} />
-        <UserDetail user={currentUser} />
+        <UsersList user={user} setUser={setSelectedUser} />
+        <Suspense fallback={<PageSpinner />}>
+          <UserDetail userID={user.id} />
+        </Suspense>
       </Fragment>
     </main>
-  );
+  ) : <PageSpinner/>;
 }
